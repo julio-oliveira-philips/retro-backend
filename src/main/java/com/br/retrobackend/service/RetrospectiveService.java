cae06841b3a3e3266eb996deed516f4f86fcff2c
Júlio Oliveira <julio.oliveira@philips.com>
2022-11-19 17:10:57 -0300

    feat: implement RetrospectiveService

---
+++ src/main/java/com/br/retrobackend/service/RetrospectiveService.java
@@ -0,0 +1,35 @@
+package com.br.retrobackend.service;
+
+import com.br.retrobackend.entitys.Retrospective;
+import com.br.retrobackend.repository.RetrospectiveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RetrospectiveService {
+
+    private final RetrospectiveRepository retrospectiveRepository;
+
+    public RetrospectiveService(RetrospectiveRepository retrospectiveRepository) {
+        super();
+        this.retrospectiveRepository = retrospectiveRepository;
+    }
+
+    public Retrospective save(Retrospective retrospective) {
+        return this.retrospectiveRepository.save(retrospective);
+    }
+
+    public List<Retrospective> getAll() {
+        return this.retrospectiveRepository.findAll();
+    }
+
+    public Optional<Retrospective> findById(int id) {
+        return this.retrospectiveRepository.findById(id);
+    }
+
+    public void deleteById(int id) {
+        this.retrospectiveRepository.deleteById(id);
+    }
+}
