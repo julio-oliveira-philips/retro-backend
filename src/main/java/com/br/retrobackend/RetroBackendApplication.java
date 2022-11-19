c27075273c84b2c3608ed86b002f8e4a595c4de6
Júlio Oliveira <julio.oliveira@philips.com>
2022-11-19 16:11:18 -0300

    initial commit

---
+++ src/main/java/com/br/retrobackend/RetroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.retrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/retrobackend/RetroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.retrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
