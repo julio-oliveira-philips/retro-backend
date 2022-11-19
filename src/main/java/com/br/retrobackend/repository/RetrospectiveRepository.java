d81b42610904d4214d460c8abacde8602d804a19
Júlio Oliveira <julio.oliveira@philips.com>
2022-11-19 16:57:31 -0300

    feat: implement RetrospectiveRepository

---
+++ src/main/java/com/br/retrobackend/repository/RetrospectiveRepository.java
@@ -0,0 +1,9 @@
+package com.br.retrobackend.repository;
+
+import com.br.retrobackend.entitys.Retrospective;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RetrospectiveRepository extends JpaRepository<Retrospective, Integer> {
+}
